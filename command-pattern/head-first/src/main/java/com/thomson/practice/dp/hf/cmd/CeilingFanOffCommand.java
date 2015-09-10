b1a7d5677d06154edc72ad2a0b6a58e12de1d67e
ThomsonTang <guiketang@gmail.com>
2015-09-10 11:09:28 +0800

    CM: Command Pattern. 增加了undo()回退操作。

---
+++ command-pattern/head-first/src/main/java/com/thomson/practice/dp/hf/cmd/CeilingFanOffCommand.java
@@ -0,0 +1,34 @@
+package com.thomson.practice.dp.hf.cmd;
+
+/**
+ * the command to turn off the ceiling fan.
+ *
+ * @author Thomson Tang
+ */
+public class CeilingFanOffCommand implements Command {
+    private CeilingFan ceilingFan;
+    private int prevSpeed;
+
+    public CeilingFanOffCommand(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+    @Override
+    public void execute() {
+        prevSpeed = ceilingFan.getSpeed();
+        ceilingFan.off();
+    }
+
+    @Override
+    public void undo() {
+        if (prevSpeed == CeilingFan.HIGH) {
+            ceilingFan.high();
+        } else if (prevSpeed == CeilingFan.MEDIUM) {
+            ceilingFan.medium();
+        } else if (prevSpeed == CeilingFan.LOW) {
+            ceilingFan.low();
+        } else if (prevSpeed == CeilingFan.OFF) {
+            ceilingFan.off();
+        }
+    }
+}
--- command-pattern/head-first/src/main/java/com/thomson/practice/dp/hf/cmd/RemoteLoader2.java
+++ command-pattern/head-first/src/main/java/com/thomson/practice/dp/hf/cmd/RemoteLoader2.java
@@ -1,12 +1,30 @@
 package com.thomson.practice.dp.hf.cmd;
 
 /**
- * Created by Intellij idea.
+ * the test for remote loader.
  *
  * @author Thomson Tang
  */
 public class RemoteLoader2 {
-    RemoteControlWithUndo remoteControl = new RemoteControlWithUndo();
+    public static void main(String[] args) {
+        RemoteControlWithUndo remoteControl = new RemoteControlWithUndo();
 
-    CeilingFan ceilingFan = new CeilingFan();
+        CeilingFan ceilingFan = new CeilingFan("Living Room");
+        CeilingFanHighCommand highCommand = new CeilingFanHighCommand(ceilingFan);
+        CeilingFanLowCommand lowCommand = new CeilingFanLowCommand(ceilingFan);
+        CeilingFanOffCommand offCommand = new CeilingFanOffCommand(ceilingFan);
+
+        remoteControl.setCommand(0, highCommand, offCommand);
+        remoteControl.setCommand(1, lowCommand, offCommand);
+
+        remoteControl.onButtonWasPushed(0);
+        remoteControl.offButtonWasPushed(1);
+        System.out.println(remoteControl);
+        remoteControl.undoButtonWasPushed();
+
+        remoteControl.onButtonWasPushed(1);
+        System.out.println(remoteControl);
+        remoteControl.undoButtonWasPushed();
+
+    }
 }
